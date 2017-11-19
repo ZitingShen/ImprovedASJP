e117f97b8152a874ed585ef9460bf8221fdc5af1
Ziting Shen <zshen@brynmawr.edu>
2017-11-18 23:13:30 -0500

    finish feature encodings; add comments

--- data_processing.py
+++ data_processing.py
@@ -3,6 +3,8 @@ import csv
 def readInRawData(input='data/Raw Swadesh List Without Url - IELEX.csv'):
 	"""
 	Read in the raw data.
+	:param input: the file name of the input
+    :return: the reader of the input file as a list
 	"""
 	with open(input) as input_file:
 		reader = csv.DictReader(input_file)
@@ -11,32 +13,38 @@ def readInRawData(input='data/Raw Swadesh List Without Url - IELEX.csv'):
 def languagesWithOrthographicForms(raw_input, \
 	output='output/Languages with Source Forms.txt'):
 	"""
-	Return a list of languages that have over 100 words with orthographical forms.
+	Return a list of languages that have over 100 words with orthographic forms.
+	:param raw_input: the raw input from a reader list
+	:param output: the file name of the output
+	:return: a sorted list of languages that has 100+ item Swadesh list in orthographic forms
 	"""
-	languages_with_orthographical_forms = {}
+	languages_with_orthographic_forms = {}
 	for item in raw_input:
-		if item['word_orthographical_form'] == '':
+		if item['word_orthographic_form'] == '':
 			continue
 		if '[Legacy]' in item['language_name']:
 			continue
-		if item['language_name'] not in languages_with_orthographical_forms:
-			languages_with_orthographical_forms[item['language_name']] = 1
+		if item['language_name'] not in languages_with_orthographic_forms:
+			languages_with_orthographic_forms[item['language_name']] = 1
 		else:
-			languages_with_orthographical_forms[item['language_name']] += 1
+			languages_with_orthographic_forms[item['language_name']] += 1
 		
 	with open(output, "w") as output_file:
-		output_file.write("Language: Number of words with orthographical forms\n\n")
-		sorted_languages = sorted(languages_with_orthographical_forms.keys())
+		output_file.write("Language: Number of words with orthographic forms\n\n")
+		sorted_languages = sorted(languages_with_orthographic_forms.keys())
 		for language in sorted_languages:
-			if languages_with_orthographical_forms[language] > 100:
+			if languages_with_orthographic_forms[language] > 100:
 				output_file.write("{}: {}\n".format(language, \
-					languages_with_orthographical_forms[language]))
+					languages_with_orthographic_forms[language]))
 		return sorted_languages
 
 def languagesWithPhonologicalForms(raw_input, \
 	output='output/Languages with phonological Forms.txt'):
 	"""
 	Return a list of languages that have over 100 words with phonological forms.
+	:param raw_input: the raw input from a reader list
+	:param output: the file name of the output
+	:return: a sorted list of languages that has 100+ item Swadesh list in phonological forms
 	"""
 	languages_with_phonological_forms = {}
 	for item in raw_input:
@@ -58,22 +66,27 @@ def languagesWithPhonologicalForms(raw_input, \
 					languages_with_phonological_forms[language]))
 		return sorted_languages
 
-def generateOrthgraphicSwadeshList(raw_input, languages_orthographical, \
-	output='data/Processed Data with Orthographical Forms - IELEX.csv'):
+def generateOrthgraphicSwadeshList(raw_input, languages_orthographic, \
+	output='data/Processed Data with Orthographic Forms - IELEX.csv'):
 	"""
-	Generate a swadesh of orthographical forms for each language that has 100+ 
-	words in orthographical forms.
+	Generate a swadesh of orthographic forms for each language that has 100+ 
+	words in orthographic forms.
+	:param raw_input: the raw input from a reader list
+	:param languages_orphographic: a sorted list of languages that has 100+ item 
+								   Swadesh list in orthographic forms
+	:param output: the file name of the output
+	:return: void
 	"""
 	with open(output, 'w') as output_file:
 		fieldnames = ['language_name', 'language_code', 'lexemes', 'word_id', \
-						'word_meaing', 'word_orthographical_form', \
+						'word_meaing', 'word_orthographic_form', \
 						'word_phonological_form', 'word_gloss', 'word_notes']
 		writer = csv.DictWriter(output_file, fieldnames=fieldnames)
 		writer.writeheader()
 
 		for item in raw_input:
-			if item['language_name'] not in languages_orthographical \
-			or item['word_orthographical_form'] == '':
+			if item['language_name'] not in languages_orthographic \
+			or item['word_orthographic_form'] == '':
 				continue
 			writer.writerow({field: item[field] for field in fieldnames})
 
@@ -82,10 +95,15 @@ def generatePhonologicalSwadeshList(raw_input, languages_phonological, \
 	"""
 	Generate a swadesh of phonological forms for each language that has 100+ 
 	words in phonological forms.
+	:param raw_input: the raw input from a reader list
+	:param languages_phonological: a sorted list of languages that has 100+ item 
+								   Swadesh list in phonological forms
+	:param output: the file name of the output
+	:return: void
 	"""
 	with open(output, 'w') as output_file:
 		fieldnames = ['language_name', 'language_code', 'lexemes', 'word_id', \
-						'word_meaing', 'word_orthographical_form', \
+						'word_meaing', 'word_orthographic_form', \
 						'word_phonological_form', 'word_gloss', 'word_notes']
 		writer = csv.DictWriter(output_file, fieldnames=fieldnames)
 		writer.writeheader()
@@ -98,7 +116,7 @@ def generatePhonologicalSwadeshList(raw_input, languages_phonological, \
 
 
 raw_input = readInRawData()
-languages_orthographical = languagesWithOrthographicForms(raw_input)
+languages_orthographic = languagesWithOrthographicForms(raw_input)
 languages_phonological = languagesWithPhonologicalForms(raw_input)
-generateOrthgraphicSwadeshList(raw_input, languages_orthographical)
+generateOrthgraphicSwadeshList(raw_input, languages_orthographic)
 generatePhonologicalSwadeshList(raw_input, languages_phonological)
--- phonemes_to_features.py
+++ phonemes_to_features.py
@@ -1,9 +1,31 @@
 import csv
 
-def readInOrthographicalData( \
-	input='data/Processed Data with Orthographical Forms - IELEX.csv'):
+class Word:
+	def __init__(self, language, meaning, representation):
+		"""
+		:param language: the language of this word
+		:param meaning: the meaning of this word
+		:param representation: the orthographic or phonological representation
+							   of this word
+		"""
+		self.language = language
+		self.meaning = meaning
+		self.representation = representation
+		self.features = getFeatures(representation)
+
+	def getFeatures(self):
+		"""
+		Convert the representation to features.
+		:return: a list of features corresponding to the representation
+		"""
+		continue
+
+def readInOrthographicData( \
+	input='data/Processed Data with Orthographic Forms - IELEX.csv'):
 	"""
-	Read in the filtered swadeshed list in the orthographical forms.
+	Read in the filtered swadeshed list in the orthographic forms.
+	:param input: the file name of the input
+	:return: the reader of the input file as a list
 	"""
 	with open(input) as input_file:
 		reader = csv.DictReader(input_file)
@@ -13,6 +35,8 @@ def readInPhonologicalData( \
 	input='data/Processed Data with Phonological Forms - IELEX.csv'):
 	"""
 	Read in the filtered swadeshed list in the phonological forms.
+	:param input: the file name of the input
+	:return: the reader of the input file as a list
 	"""
 	with open(input) as input_file:
 		reader = csv.DictReader(input_file)
@@ -21,6 +45,9 @@ def readInPhonologicalData( \
 def checkIPASymbols(input, output='output/IPA Symbols - IELEX.txt'):
 	"""
 	Check the range of IPA symbols of the phonological data.
+	:param input: the reader of the input file as a list
+	:param output: the file name of the output
+	:return: a list of IPA symbols used in the phonological data
 	"""
 	symbols = {}
 	for item in input:
@@ -34,4 +61,3 @@ def checkIPASymbols(input, output='output/IPA Symbols - IELEX.txt'):
 				symbols[symbol]))
 	return symbols
 
-checkIPASymbols(readInPhonologicalData())
